593cf75a2c21ee52227fc46e1a9f2ec42ebb9bc8
shijiaxin <shijiaxin.cn@gmail.com>
2016-12-26 21:10:52 +0800

    finish Update rules

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -59,13 +59,14 @@ def sgd_momentum(w, dw, config=None):
   config.setdefault('learning_rate', 1e-2)
   config.setdefault('momentum', 0.9)
   v = config.get('velocity', np.zeros_like(w))
-  
+
   next_w = None
   #############################################################################
   # TODO: Implement the momentum update formula. Store the updated value in   #
   # the next_w variable. You should also use and update the velocity v.       #
   #############################################################################
-  pass
+  v=config['momentum']*v- config['learning_rate'] * dw
+  next_w =w+v
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -96,10 +97,11 @@ def rmsprop(x, dx, config=None):
   next_x = None
   #############################################################################
   # TODO: Implement the RMSprop update formula, storing the next value of x   #
-  # in the next_x variable. Don't forget to update cache value stored in      #  
+  # in the next_x variable. Don't forget to update cache value stored in      #
   # config['cache'].                                                          #
   #############################################################################
-  pass
+  config['cache']=config['decay_rate']*config['cache']+(1-config['decay_rate'])*dx*dx;
+  next_x = x-config['learning_rate']/np.sqrt(config['cache']+config['epsilon'])*dx;
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -129,21 +131,21 @@ def adam(x, dx, config=None):
   config.setdefault('m', np.zeros_like(x))
   config.setdefault('v', np.zeros_like(x))
   config.setdefault('t', 0)
-  
+
   next_x = None
   #############################################################################
   # TODO: Implement the Adam update formula, storing the next value of x in   #
   # the next_x variable. Don't forget to update the m, v, and t variables     #
   # stored in config.                                                         #
   #############################################################################
-  pass
+  config['t']=config['t']+1;
+  config['m']=config['beta1']*config['m']+(1-config['beta1'])*dx;
+  config['v']=config['beta2']*config['v']+(1-config['beta2'])*dx*dx;
+  m_hat= config['m']/(1-config['beta1']**config['t']);
+  v_hat= config['v']/(1-config['beta2']**config['t']);
+  next_x = x -config['learning_rate']/(np.sqrt(v_hat)+config['epsilon'])*m_hat;
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
-  
+
   return next_x, config
-
-  
-  
-  
-
--- note_assignment2.md
+++ note_assignment2.md
@@ -8,4 +8,26 @@ dx = dout*(x>0)
 #### Multilayer network
 Two-Layter的时候可以写para['W1'],para['W2']这样的参数  
 但是layer是变量的时候不方便  
-所以改为para[('W',1)]，以tuple为key
+所以改为para[('W',1)]，以tuple为key  
+#### Update rules
+可以参考http://sebastianruder.com/optimizing-gradient-descent/  
+
+SGD公式如下:  
+$v=- r * dW;$  
+$W=W+v;$  
+
+SGD+Momentum则是表明物体有一定惯性朝着原有方向前进，公式如下:  
+$v=\gamma *v-r * dW;$  
+$W=W+v;$  
+这里 $\gamma$ 表示原有速度损失了一定比例  
+
+RMSprop的公式如下：  
+$Cache[g^2]=0.9*Cache[g^2]+0.1*g_t^2$  
+$W=W-\frac{\eta}{\sqrt{Cache[g^2]+ \epsilon}} * g_t$  
+
+Adam公式如下:  
+$m_t=β_1m_{t−1}+(1−β_1)g_t$  
+$v_t=β_2v_{t−1}+(1−β_2)g_t^2$  
+$\hat{m}_t=\frac{m_t}{1-β_1^t}$  
+$\hat{v}_t=\frac {v_t} {1-β_2^t}$  
+$W=W-\frac{\eta}{\sqrt{\hat{v}_t}+\epsilon}*\hat{m}_t$
